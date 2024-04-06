b10ad8f2bb111df509df7899454c293e2c2141b0
Maks <maks210306@yandex.by>
2024-04-07 01:03:29 +0300

    GeneratePassworPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import * as Components from "./Components";
 import axios from "axios";
 import Navbar from "./Navbar";
+import "./generatingButton.css";
+import Generate from "./generate";
+
 function App() {
     const [signIn, toggle] = useState(true);
     const [username, setUsername] = useState("");
@@ -10,31 +13,40 @@ function App() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [redirect, setRedirect] = useState(false);
 
     const handleSignUp = async (e) => {
         e.preventDefault();
 
-        const response = await axios.post("http://localhost:8080/api/auth/signup", {
-            username: name,
-            email,
-            password,
-        });
+        try {
+            const response = await axios.post("http://localhost:8080/api/auth/signup", {
+                username: name,
+                email,
+                password,
+            });
 
-        console.log(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     const handleSignIn = async (e) => {
         e.preventDefault();
 
-        const response = await axios.post("http://localhost:8080/api/auth/signin", {
-            username,
-            password,
-        });
+        try {
+            const response = await axios.post("http://localhost:8080/api/auth/signin", {
+                username,
+                password,
+            });
 
-        console.log(response.data);
-        localStorage.setItem("token", response.data.token);
-        setUsername(response.data.username);
-        setIsAuthenticated(true);
+            console.log(response.data);
+            localStorage.setItem("token", response.data.token);
+            setUsername(response.data.username);
+            setIsAuthenticated(true);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     const handleLogout = () => {
@@ -43,81 +55,102 @@ function App() {
         setIsAuthenticated(false);
     };
 
+    const handleGeneratePassword = () => {
+        setRedirect(true);
+    };
+
+    const handleReturnToAuth = (value) => {
+        setRedirect(value);
+    };
+
     return (
         <Router>
-        <>
-            <Navbar username={username} onLogout={handleLogout} isAuthenticated={isAuthenticated} />
-        <Components.Container>
-            <Components.SignUpContainer signinIn={signIn}>
-                <Components.Form onSubmit={handleSignUp}>
-                    <Components.Title>Create Account</Components.Title>
-                    <Components.Input
-                        type="text"
-                        placeholder="Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+            {redirect ? (
+                <Generate onReturnToAuth={handleReturnToAuth} />
+            ) : (
+                <>
+                    <Navbar
+                        username={username}
+                        onLogout={handleLogout}
+                        isAuthenticated={isAuthenticated}
                     />
-                    <Components.Input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <Components.Input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Components.Button type="submit">Sign Up</Components.Button>
-                </Components.Form>
-            </Components.SignUpContainer>
+                    <Components.Container>
+                        <Components.SignUpContainer signinIn={signIn}>
+                            <Components.Form onSubmit={handleSignUp}>
+                                <Components.Title>Create Account</Components.Title>
+                                <Components.Input
+                                    type="text"
+                                    placeholder="Name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                <Components.Input
+                                    type="email"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <Components.Input
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <Components.Button type="submit">Sign Up</Components.Button>
+                            </Components.Form>
+                        </Components.SignUpContainer>
 
-            <Components.SignInContainer signinIn={signIn}>
-                <Components.Form onSubmit={handleSignIn}>
-                    <Components.Title>Sign in</Components.Title>
-                    <Components.Input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <Components.Input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Components.Anchor href="#">Forgot your password?</Components.Anchor>
-                    <Components.Button type="submit">Sign In</Components.Button>
-                </Components.Form>
-            </Components.SignInContainer>
+                        <Components.SignInContainer signinIn={signIn}>
+                            <Components.Form onSubmit={handleSignIn}>
+                                <Components.Title>Sign in</Components.Title>
+                                <Components.Input
+                                    type="text"
+                                    placeholder="Username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                                <Components.Input
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <Components.Anchor href="#">Forgot your password?</Components.Anchor>
+                                <Components.Button type="submit">Sign In</Components.Button>
+                            </Components.Form>
+                        </Components.SignInContainer>
 
-            <Components.OverlayContainer signinIn={signIn}>
-                <Components.Overlay signinIn={signIn}>
-                    <Components.LeftOverlayPanel signinIn={signIn}>
-                        <Components.Title>Welcome Back!</Components.Title>
-                        <Components.Paragraph>
-                            To keep connected with us please login with your personal info
-                        </Components.Paragraph>
-                        <Components.GhostButton onClick={() => toggle(true)}>
-                            Sign In
-                        </Components.GhostButton>
-                    </Components.LeftOverlayPanel>
+                        <Components.OverlayContainer signinIn={signIn}>
+                            <Components.Overlay signinIn={signIn}>
+                                <Components.LeftOverlayPanel signinIn={signIn}>
+                                    <Components.Title>Welcome Back!</Components.Title>
+                                    <Components.Paragraph>
+                                        To keep connected with us please login with your personal info
+                                    </Components.Paragraph>
+                                    <Components.GhostButton onClick={() => toggle(true)}>
+                                        Sign In
+                                    </Components.GhostButton>
+                                </Components.LeftOverlayPanel>
 
-                    <Components.RightOverlayPanel signinIn={signIn}>
-                        <Components.Title>Hello, Friend!</Components.Title>
-                        <Components.Paragraph>
-                            Enter Your personal details and start journey with us
-                        </Components.Paragraph>
-                        <Components.GhostButton onClick={() => toggle(false)}>
-                            Sign Up
-                        </Components.GhostButton>
-                    </Components.RightOverlayPanel>
-                </Components.Overlay>
-            </Components.OverlayContainer>
-        </Components.Container>
-        </>
+                                <Components.RightOverlayPanel signinIn={signIn}>
+                                    <Components.Title>Hello, Friend!</Components.Title>
+                                    <Components.Paragraph>
+                                        Enter Your personal details and start journey with us
+                                    </Components.Paragraph>
+                                    <Components.GhostButton onClick={() => toggle(false)}>
+                                        Sign Up
+                                    </Components.GhostButton>
+                                </Components.RightOverlayPanel>
+                            </Components.Overlay>
+                        </Components.OverlayContainer>
+                    </Components.Container>
+                    <div className="container_to_but">
+                        <button onClick={handleGeneratePassword} className="button_to_generate">
+                            Сгенерировать пароль
+                        </button>
+                    </div>
+                </>
+            )}
         </Router>
     );
 }
---
+++ src/generate.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import "./generatingButton.css";
+import axios from "axios";
+
+const Generate = ({ onReturnToAuth }) => {
+    const [passwordLevel, setPasswordLevel] = useState("");
+    const [characterCount, setCharacterCount] = useState("");
+    const [generatedPassword, setGeneratedPassword] = useState("");
+    const token = localStorage.getItem("token");
+
+    const handlePasswordLevelChange = (e) => {
+        setPasswordLevel(e.target.value);
+    };
+
+    const handleCharacterCountChange = (e) => {
+        setCharacterCount(e.target.value);
+    };
+
+    const handleGeneratePassword = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(
+                "http://localhost:8080/api/password/generatePassword",
+                {
+                    difficulty: passwordLevel,
+                    length: characterCount,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const generatedPassword = response.data;
+            setGeneratedPassword(generatedPassword);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleReturnToAuth = () => {
+        onReturnToAuth(false);
+    };
+
+    return (
+        <div className="PasswordPage">
+            <div className="exit" onClick={handleReturnToAuth}>Вернуться к авторизации</div>
+            <form onSubmit={handleGeneratePassword}>
+                <button className="button_to_generate" type="submit">Сгенерировать пароль</button>
+                <label>
+                    Выберите уровень сложности пароля:
+                    <select value={passwordLevel} onChange={handlePasswordLevelChange}>
+                        <option value="EASY">Выбор</option>
+                        <option value="EASY">EASY</option>
+                        <option value="NORMAL">NORMAL</option>
+                        <option value="HARD">HARD</option>
+                    </select>
+                </label>
+                <label>
+                    Введите количество символов (до 255):
+                    <input type="text" value={characterCount} onChange={handleCharacterCountChange} />
+                </label>
+            </form>
+            <h1 className="genereativeH1">Ваш пароль: {generatedPassword}</h1>
+        </div>
+    );
+};
+
+export default Generate;
